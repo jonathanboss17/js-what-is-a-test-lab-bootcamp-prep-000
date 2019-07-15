749ad7507ccccdf28adb61f34121a57446580143
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-15 10:59:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 39
-var message = ` is ${height} inches taller than Joe`
+var message = `${name} is ${height} inches taller than Joe`
 
 module.exports = {name, height, message}
